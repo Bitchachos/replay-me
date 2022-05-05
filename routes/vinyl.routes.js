251bed2796431c9a1bea13d620dcf81ec3dcb72c
Lukecin <dluqueru@gmail.com>
2022-05-05 17:06:31 +0200

    safe commit

--- routes/vinyl.routes.js
+++ routes/vinyl.routes.js
@@ -61,11 +61,10 @@ router.get("/vinyl", (req, res, next) => {
 router.get("/vinyl/:vinylId", (req, res, next) => {
     const id = req.params.vinylId;
     let ownerUser = false
-    console.log("stringgu");
     Vinyl.findById(id)
     .populate("owner")
     .then( (vinylDetails) => {
-        console.log("HEYHEY", vinylDetails);
+
         if(req.session.user?._id === vinylDetails.owner._id.toString()) {ownerUser = true}
         res.render("vinyl/vinyl-details", {vinylDetails, ownerUser})
     })
